42c6594f129cb67f1c443c182aafb102530b32b6
Adam Kercheval <arkercheval@gmail.com>
2018-05-07 16:43:57 -0400

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+set fish_greeting ""
+alias deb='ssh adam@ssh.akerch.com'
+alias praline='ssh adam@praline.wwmairs.com'
+alias up='sudo apt-get -y update; sudo apt-get -y upgrade'
+alias open='xdg-open'
+alias espy='/home/adam/Documents/espy/pypy-c'
+alias bye='openbox --exit'
+alias ciao='sudo shutdown now'
+
+thefuck --alias | source
